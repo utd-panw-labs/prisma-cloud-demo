2f84f129cd93ba34a4daa71363165766b73f2b0b
Ubuntu <ubuntu@suhas-compute.openstack.internal>
2023-04-05 02:17:28 +0000

    updates

--- terraform/aws/modules/security_group/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "port" {
-
-}
-
-variable "cidrs" {
-  
-}
--- terraform/azure/resource_group.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_resource_group" "example" {
-  name     = "tg-${var.environment}"
-  location = var.location
-  tags = {
-    git_file             = "terraform/azure/resource_group.tf"
-  }
-}
